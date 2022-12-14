eec116a482e4707034e139b355f12e47a46ba5ba
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2022-12-14 06:10:15 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * of a number
+ *
+ * @n: input number as an integer.
+ *
+ * Return: Last digit
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (1);
+	}
+}
