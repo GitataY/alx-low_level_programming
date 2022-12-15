d9326f056c60b70710f1ab35ff0102cf435ed60f
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2022-12-15 00:34:58 -0800

    main

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -causes an infinirte loop
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
