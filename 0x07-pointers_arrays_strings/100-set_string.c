0636207014340f3e2de26495c7cba8ff774091dc
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-01-03 14:12:02 -0800

    stringset

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: source address
+ * @to: the target address
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
