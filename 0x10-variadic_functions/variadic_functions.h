190be2595427e372fea53ac79a330032409a3c1c
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-01-20 00:23:30 -0800

    first commit

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VAR_FUNCTIONS
+#define VAR_FUNCTIONS
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
