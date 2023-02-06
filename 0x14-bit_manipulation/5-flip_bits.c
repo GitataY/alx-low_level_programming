43f2f016186c967d2848099cd96b7199789e5862
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2023-02-06 08:50:06 -0800

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits from one number to another
+ * @n: first bits
+ * @m: second bit
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int kbits;
+
+	for (kbits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			kbits++;
+	}
+
+	return (kbits);
+}
